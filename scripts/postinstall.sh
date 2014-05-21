1d8f0a0389f3d9974aa8ec7d91febf5d97fdf2ff
Balaji Narain <narainbalaji@gmail.com>
2014-05-21 17:28:09 +0530

    Utsab, Balaji | #77 | Temp commit

---
+++ scripts/postinstall.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+
+#create links
+
+ln -s /var/lib/bdshr-terminology-server/etc /etc/bdshr-terminology-server
+ln -s /var/lib/bdshr-terminology-server/bin/bdshr-terminology-server /etc/init.d/bdshr-terminology-server
+ln -s /var/lib/bdshr-terminology-server/run /var/run/bdshr-terminology-server
+ln -s /var/lib/bdshr-terminology-server/log /var/log/bdshr-terminology-server
+
+(cd /var/lib/bdshr-terminology-server/openmrs && unzip ../openmrs.war)
+
+chkconfig --add bdshr-terminology-server
---
+++ scripts/preuninstall.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+#Clean up
+rm -rf /var/lib/bdshr-terminology-server
+
+rm -f /etc/bdshr-terminology-server
+rm -f /etc/init.d/bdshr-terminology-server
+rm -f /var/run/bdshr-terminology-server
+rm -f /var/log/bdshr-terminology-server
+
+
+true || service bdshr-terminology-server stop
