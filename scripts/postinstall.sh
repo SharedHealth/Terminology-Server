f6cc16468bb8e3c2273f60c21f92781239b9206e
Balaji Narain <narainbalaji@gmail.com>
2014-05-21 18:18:25 +0530

    Utsab, BalajiN | #77 | Correcting post install

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -6,6 +6,7 @@
 ln -s /var/lib/bdshr-terminology-server/etc /etc/bdshr-terminology-server
 ln -s /var/lib/bdshr-terminology-server/bin/bdshr-terminology-server /etc/init.d/bdshr-terminology-server
 ln -s /var/lib/bdshr-terminology-server/run /var/run/bdshr-terminology-server
+ln -s /var/lib/bdshr-terminology-server/openmrs /var/run/bdshr-terminology-server/openmrs
 ln -s /var/lib/bdshr-terminology-server/log /var/log/bdshr-terminology-server
 
 (cd /var/lib/bdshr-terminology-server/openmrs && unzip ../openmrs.war)
