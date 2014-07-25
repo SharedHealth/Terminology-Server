ca310e5039e9f70f33eb0e480e59bdc6303684ac
Balaji Narain <balajin@thoughtworks.com>
2014-07-25 11:17:34 +0530

    BalajiN | Fixing cleanup steps

---
+++ scripts/postuninstall.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+rm -rf /var/lib/bdshr-terminology-server
+
+rm -rf /etc/bdshr-terminology-server
+rm -rf /etc/init.d/bdshr-terminology-server
+rm -rf /var/run/bdshr-terminology-server
+rm -rf /var/log/bdshr-terminology-server
+
+
--- scripts/preuninstall.sh
+++ scripts/preuninstall.sh
@@ -3,12 +3,4 @@
 #stop the server
 service bdshr-terminology-server stop || true
 
-#Clean up
-rm -rf /var/lib/bdshr-terminology-server
-
-rm -f /etc/bdshr-terminology-server
-rm -f /etc/init.d/bdshr-terminology-server
-rm -f /var/run/bdshr-terminology-server
-rm -f /var/log/bdshr-terminology-server
-
 
