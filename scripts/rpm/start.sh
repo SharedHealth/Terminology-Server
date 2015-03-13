830b65f098bf8ab356e045ef706d47ca3d2245fe
rnjn <runjan@gmail.com>
2015-03-13 17:06:07 +0530

    Configuration for service run updated with new folder location of binaries

--- scripts/rpm/start.sh
+++ scripts/rpm/start.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-nohup java -jar $SERVER_OPTS /var/lib/bdshr-terminology-server/lib/bdshr-terminology-server.jar > /var/log/bdshr-terminology-server/bdshr-terminology-server.log 2>/var/log/bdshr-terminology-server/bdshr-terminology-server.log < /dev/null &
+nohup java -jar $SERVER_OPTS /opt/bdshr-terminology-server/lib/bdshr-terminology-server.jar > /var/log/bdshr-terminology-server/bdshr-terminology-server.log 2>/var/log/bdshr-terminology-server/bdshr-terminology-server.log < /dev/null &
 echo $! > /var/run/bdshr-terminology-server/bdshr-terminology-server.pid
